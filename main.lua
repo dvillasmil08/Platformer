6a20503d4364e82ebe740eb742ab516fa2a4c420
dvillasmil08 <dvillasmil08@gmail.com>
2022-01-21 19:14:18 -0500

    player moves left/right and jumps with arrows

--- main.lua
+++ main.lua
@@ -3,14 +3,33 @@ function love.load()
     world = wf.newWorld(0, 800)
 
     player = world:newRectangleCollider(360, 100, 80, 80)
+    player.speed = 240
+
     platform = world:newRectangleCollider(250, 400,  300, 100)
     platform:setType('static')
 end
 
 function love.update(dt)
     world:update(dt)
+
+    local px, py = player:getPosition()
+    -- could change right arrow to D
+    if love.keyboard.isDown('right') then
+        player:setX(px + player.speed*dt)
+    end
+    -- could change right arrow to A
+    if love.keyboard.isDown('left') then
+        player:setX(px - player.speed*dt)
+    end
 end
 
 function love.draw()
     world:draw()
+end
+
+function love.keypressed(key)
+    -- Could change up arrow to W
+    if key == 'up' then
+        player:applyLinearImpulse(0, -7000)
+    end
 end
