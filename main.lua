e24acd73c9a8e1ec00cd26970d186e6c50fd537e
dvillasmil08 <dvillasmil08@gmail.com>
2022-01-21 19:05:01 -0500

    added platform and draw

--- main.lua
+++ main.lua
@@ -1,12 +1,15 @@
 function love.load()
     wf = require 'Libraries/windfield'
-    world = wf.newWorld(0, 10gi0)
+    world = wf.newWorld(0, 800)
+
+    player = world:newRectangleCollider(360, 100, 80, 80)
+    platform = world:newRectangleCollider(250, 400,  300, 100)
 end
 
 function love.update(dt)
-
+    world:update(dt)
 end
 
 function love.draw()
-
+    world:draw()
 end
